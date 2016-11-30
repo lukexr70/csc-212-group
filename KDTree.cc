d338d22bcf04b9b2496f16e99a960335672991c3
Douglas Deslauriers <DeslauriersDouglas@gmail.com>
2016-11-30 16:04:09 -0500

    Added size and depth

--- KDTree.cc
+++ KDTree.cc
@@ -2,50 +2,51 @@
 #include <math.h>
 
 KDNode::KDNode(double lat, double lon, const char *desc) {
-    left = NULL;
-    right = NULL;
-    description = desc;
-    latitude = lat;
-    longitude = lon;
+	left = NULL;
+	right = NULL;
+	description = desc;
+	latitude = lat;
+	longitude = lon;
 }
 
 KDNode::~KDNode() {
 }
 
 double KDNode::distance(double lat, double lon) {
-    double param = M_PI / 180.0; // required for conversion from degrees to radians
-    double rad = 3956.0;  // radius of earth in miles
-    double d_lat = (lat - latitude) * param;
-    double d_lon = (lon - longitude) * param;
-    double dist = sin(d_lat/2) * sin(d_lat/2) + cos(latitude*param) * cos(lat*param) * sin(d_lon/2) * sin(d_lon/2);
-    dist = 2.0 * atan2(sqrt(dist), sqrt(1-dist));
-    return rad * dist;
+	double param = M_PI / 180.0; // required for conversion from degrees to radians
+	double rad = 3956.0;  // radius of earth in miles
+	double d_lat = (lat - latitude) * param;
+	double d_lon = (lon - longitude) * param;
+	double dist = sin(d_lat/2) * sin(d_lat/2) + cos(latitude*param) * cos(lat*param) * sin(d_lon/2) * sin(d_lon/2);
+	dist = 2.0 * atan2(sqrt(dist), sqrt(1-dist));
+	return rad * dist;
 }
 
 KDTree::KDTree() {
-    root = NULL;
-    size = 0;
+	root = NULL;
+	size = 0;
 }
 
 KDTree::~KDTree() {
-    // TODO
+	destroy(root);	
 }
 
 void KDTree::destroy(KDNode *p) {
-    // TODO
+	size = 0;
+	//Traversal
+
 }
 
 
 void KDTree::insert(double lat, double lon, const char *desc) {
-    // TODO
+	// TODO
 }
 
 unsigned int KDTree::printNeighbors(double lat, double lon, double rad, const char *filter) {
-    // TODO
-    return -1;
+	// TODO
+	return -1;
 }
 
 unsigned int KDTree::getSize() {
-    // TODO
-    return -1;
+	return size;
 }
--- KDTree.h
+++ KDTree.h
@@ -3,39 +3,39 @@
 #include <string>
 
 class KDNode {
-    private:
-        double latitude;
-        double longitude;
-        std::string description;
-        KDNode *left;
-        KDNode *right;
-        int depth;
+	private:
+	    double latitude;
+	    double longitude;
+	    std::string description;
+	    KDNode *left;
+	    KDNode *right;
+	    int depth;
 
-        // this function returs the distance in miles from lat, lon
-        // to this object
-        double distance(double lat, double lon);
+	    // this function returs the distance in miles from lat, lon
+	    // to this object
+	    double distance(double lat, double lon);
 
-    public:
-        KDNode(double lat, double lon, const char *desc);
-        ~KDNode();
+	public:
+	    KDNode(double lat, double lon, const char *desc);
+	    ~KDNode();
 
-    friend class KDTree;
+	friend class KDTree;
 };
 
 class KDTree {
-    private:
-        unsigned int size;
-        KDNode *root;
+	private:
+	    unsigned int size;
+	    KDNode *root;
 
-        void destroy(KDNode *p);
+	    void destroy(KDNode *p);
 
-    public:
-        KDTree();
-        ~KDTree();
+	public:
+	    KDTree();
+	    ~KDTree();
 
-        unsigned int getSize();
-        void insert(double lat, double lon, const char *desc);
-        unsigned int printNeighbors(double lat, double lon, double rad, const char *filter);
+	    unsigned int getSize();
+	    void insert(double lat, double lon, const char *desc);
+	    unsigned int printNeighbors(double lat, double lon, double rad, const char *filter);
 };
 
 #endif
